f460e508f13ba87b81a9d8dd95bd29fa30d98ba6
Costa <ricminde@gmail.com>
2024-07-18 17:02:59 +0100

    Versão 1

---
+++ Empreendimento Turistico/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Empreendimento_Turistico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("--------------------- Empreendimentos Boa Vida -------------------------------");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+
+            Console.WriteLine("Qual o nome do Hospede?");
+            string nomeHospede = Console.ReadLine();
+
+            string[,] tipoApart =
+            {
+            {"Apartamento A","150.00" },
+            {"Apartamento B","75.00" },
+            {"Apartamento C","100.00" },
+            {"Apartamento D","150.00" },
+            };
+
+            string[,] artigosBar =
+            {
+            {"Café","1" },
+            {"Chá","1" },
+            {"Água","1" },
+            {"Leite","1" },
+            {"Iogurte","1" },
+            {"Sumo","1" },
+
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
